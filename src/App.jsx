3899fe70c0c93a45ad72c40b6e947f415c73a7b0
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-11-25 14:30:38 -0300

    fix: corrigiendo estilos del boton nav en celular

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
           <Main />
           {/* boton navegacion de celular */}
           <article
-            className="bg-[#9e9d9df5]   backdrop-blur-3xl max-w-maximoAnchosmallwide
-            md:hidden fixed  mx-auto left-7 right-7 rounded-xl bottom-3 h-[8dvh] z-index-sup  ">
+            className=" bg-[#ffffff13]  backdrop-blur-3xl max-w-maximoAnchosmallwide
+            md:hidden fixed  mx-auto left-[2.75rem] right-[2.75rem] rounded-xl bottom-3 h-[8dvh] z-index-sup  ">
             <div className="nav-button-mob w-full flex flex-wrap justify-around content-center h-[8vh] p-3">
               <img onClick={NavBar} id="boton-menu" className="w-[4rem] z-40"
                 src="../../assets/icono-monitor-code.png" alt=""></img>
