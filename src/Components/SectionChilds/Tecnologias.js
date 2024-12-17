d688f4cfb5631e993f7c71b560096ef4303769f8
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-12-17 10:23:17 -0300

    fix: rutas de icono astro e imagenes proyectos

--- src/Components/SectionChilds/Tecnologias.js
+++ src/Components/SectionChilds/Tecnologias.js
@@ -1,38 +1,38 @@
 export const ArrayTech = [
-  { id: 4355354, tecnologia: "Html", img: "/src/assets/html.svg" },
-  { id: 435565645354, tecnologia: "Css", img: "/src/assets/css.svg" },
+  { id: 4355354, tecnologia: "Html", img: "/public/assets/html.svg" },
+  { id: 435565645354, tecnologia: "Css", img: "/public/assets/html.svg" },
   {
     id: 4355656453555644,
     tecnologia: "JavaScript",
-    img: "/src/assets/js.svg",
+    img: "/public/assets/js.svg",
   },
-  { id: 43556564345354, tecnologia: "React Js", img: "/src/assets/react.svg" },
-  { id: 6555565, tecnologia: "Astro Js", img: "/src/assets/Astro.svg" },
+  { id: 43556564345354, tecnologia: "React Js", img: "/public/assets/react.svg" },
+  { id: 6555565, tecnologia: "Astro Js", img: "/public/assets/Astro.svg" },
   {
     id: 4545565645354,
     tecnologia: "Tailwind",
-    img: "/src/assets/tailwind.svg",
+    img: "/public/assets/tailwind.svg",
   },
-  { id: 43545354, tecnologia: "Sass", img: "/src/assets/sass.svg" },
-  { id: 343435565645354, tecnologia: "Git", img: "/src/assets/git.svg" },
+  { id: 43545354, tecnologia: "Sass", img: "/public/assets/sass.svg" },
+  { id: 343435565645354, tecnologia: "Git", img: "/public/assets/git.svg" },
 
-  { id: 4355656345354, img: "/src/assets/figma.svg", tecnologia: "Figma" },
+  { id: 4355656345354, img: "/public/assets/figma.svg", tecnologia: "Figma" },
 
-  { id: 4355656453504, img: "/src/assets/wix.svg" },
-  { id: 76876867, tecnologia: "TypeScript", img: "/src/assets/iconoTS.svg" },
+  { id: 4355656453504, img: "/public/assets/wix.svg" },
+  { id: 76876867, tecnologia: "TypeScript", img: "/public/assets/iconoTS.svg" },
   {
     id: 43653453453504,
     tecnologia: "ReduxToolkit",
-    img: "/src/assets/iconoRedux.svg",
+    img: "/public/assets/iconoRedux.svg",
   },
   {
     id: 34576656453504,
     tecnologia: "ReactQuery",
-    img: "/src/assets/iconoReactQ.svg",
+    img: "/public/assets/iconoReactQ.svg",
   },
   {
     id: 845345656453504,
     tecnologia: "Firebase",
-    img: "/src/assets/iconoFirebase.svg",
+    img: "/public/assets/iconoFirebase.svg",
   },
 ];
