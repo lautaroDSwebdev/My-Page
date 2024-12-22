527099a0fe09bc361091281007fd1fd2cfba080f
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-12-21 22:31:21 -0300

    test de imagenes diferentes rutas

--- src/mock/Tecnologias.js
+++ src/mock/Tecnologias.js
@@ -1,41 +1,41 @@
  const ArrayTech = [
   // ../../src/assets
   // ../../public/
-  { id: 4355354, tecnologia: "Html", img: "../../public/html.svg" },
-  { id: 435565645354, tecnologia: "Css", img: "../../public/css.svg" },
+  { id: 4355354, tecnologia: "Html", img: "../../public/assets/html.svg" },
+  { id: 435565645354, tecnologia: "Css", img: "../../public/assets/css.svg" },
   {
     id: 4355656453555644,
     tecnologia: "JavaScript",
-    img: "../../public/js.svg",
+    img: "../../public/assets/js.svg",
   },
-  { id: 43556564345354, tecnologia: "React Js", img: "../../public/react.svg" },
-  { id: 6555565, tecnologia: "Astro Js", img: "../../public/Astro.svg" },
+  { id: 43556564345354, tecnologia: "React Js", img: "../../public/assets/react.svg" },
+  { id: 6555565, tecnologia: "Astro Js", img: "../../public/assets/Astro.svg" },
   {
     id: 4545565645354,
     tecnologia: "Tailwind",
-    img: "../../public/tailwind.svg",
+    img: "../../public/assets/tailwind.svg",
   },
-  { id: 43545354, tecnologia: "Sass", img: "../../public/sass.svg" },
-  { id: 343435565645354, tecnologia: "Git", img: "../../public/git.svg" },
+  { id: 43545354, tecnologia: "Sass", img: "../../public/assets/sass.svg" },
+  { id: 343435565645354, tecnologia: "Git", img: "../../public/assets/git.svg" },
 
-  { id: 4355656345354, img: "../../public/figma.svg", tecnologia: "Figma" },
+  { id: 4355656345354, img: "../../public/assets/figma.svg", tecnologia: "Figma" },
 
-  { id: 4355656453504, img: "../../public/wix.svg" },
-  { id: 76876867, tecnologia: "TypeScript", img: "../../public/iconoTS.svg" },
+  { id: 4355656453504, img: "../../public/assets/wix.svg" },
+  { id: 76876867, tecnologia: "TypeScript", img: "../../public/assets/iconoTS.svg" },
   {
     id: 43653453453504,
     tecnologia: "ReduxToolkit",
-    img: "../../public/iconoRedux.svg",
+    img: "../../public/assets/iconoRedux.svg",
   },
   {
     id: 34576656453504,
     tecnologia: "ReactQuery",
-    img: "../../public/iconoReactQ.svg",
+    img: "../../public/assets/iconoReactQ.svg",
   },
   {
     id: 845345656453504,
     tecnologia: "Firebase",
-    img: "../../public/iconoFirebase.svg",
+    img: "../../public/assets/iconoFirebase.svg",
   },
 ];
 export default ArrayTech
